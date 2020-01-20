41ae423ffd23ed5a2a52790695ba1b5c895c4f61
brandon moro <brandon_078@hotmail.com>
2020-01-20 22:01:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -121,6 +121,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 alan_key = programmer_hash[:alan_kay][:known_for]
-	  alan_key[0]
+	programmer_hash[:alan_kay][:known_for]
 end
